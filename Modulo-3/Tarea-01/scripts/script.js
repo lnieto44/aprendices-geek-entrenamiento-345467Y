ec42726b22d7c010e88b2c3b13a3624dddfae4f4
Yineth Duarte <yinethpao170@gmail.com>
2022-02-06 21:10:52 -0500

    Tarea 01 terminada

---
+++ Modulo-3/Tarea-01/scripts/script.js
@@ -0,0 +1,119 @@
+function cambiar() {
+
+
+    let primerSeleccionador = document.getElementById("dropdown");
+    //console.log(select1.value)en .value trae el valor que hemos elegido
+    let primerValor = primerSeleccionador.value;//para poder manipular el valor del seleccionador
+   
+    let segundoSeleccionador = document.getElementById("dropdown2")
+    let segundoValor = segundoSeleccionador.value;
+
+
+    let ingresoDeMoneda = document.getElementById("ingresoMoneda");
+    let ingresoValor = ingresoDeMoneda.value;
+
+    if(ingresoValor<=0){
+        let validar = document.getElementById("val");
+        validar.className = "validacion";
+        validar.innerText="Ingrese un valor válido (Mayor a 0)";
+    } else{
+        let validar = document.getElementById("val");
+        validar.className = "validacion ocultar";
+    }
+
+    let valorSalida = 0;
+    
+
+    if(primerValor ==  'Dolar' && segundoValor == 'Peso Colombiano'){
+        valorSalida = ingresoValor * 4000;
+    }
+    if(primerValor ==  'Peso Colombiano' && segundoValor == 'Dolar'){
+        valorSalida = ingresoValor / 4000;
+    }
+    if(primerValor ==  'Peso Colombiano' && segundoValor == 'Euro' ){
+        valorSalida = ingresoValor / 4526;
+    }
+    if(primerValor ==  'Euro' && segundoValor == 'Peso Colombiano' ){
+        valorSalida = ingresoValor * 4526;
+    }
+    if(primerValor ==  'Peso Colombiano' && segundoValor == 'Libra Esterlina' ){
+        valorSalida = ingresoValor / 5349;
+    }
+    if(primerValor ==  'Libra Esterlina' && segundoValor == 'Peso Colombiano' ){
+        valorSalida = ingresoValor * 5349;
+    }
+    if(primerValor ==  'Peso Colombiano' && segundoValor == 'Peso Mexicano' ){
+        valorSalida = ingresoValor / 191;
+    }
+    if(primerValor ==  'Peso Mexicano' && segundoValor == 'Peso Colombiano' ){
+        valorSalida = ingresoValor * 191;
+    }
+    if(primerValor ==  'Dolar' && segundoValor == 'Peso Mexicano' ){
+        valorSalida = ingresoValor * 20.68;
+    }
+    if(primerValor ==  'Peso Mexicano' && segundoValor == 'Dolar' ){
+        valorSalida = ingresoValor / 20.68;
+    }
+    if(primerValor ==  'Dolar' && segundoValor == 'Euro' ){
+        valorSalida = ingresoValor  / 1.14;
+    }
+    if(primerValor == 'Euro'  && segundoValor == 'Dolar' ){
+        valorSalida = ingresoValor  * 1.14;
+    }
+    if(primerValor ==  'Dolar' && segundoValor == 'Libra Esterlina'){
+        valorSalida = ingresoValor  / 1.35;
+    }
+    if(primerValor == 'Libra Esterlina' && segundoValor == 'Dolar' ){
+        valorSalida = ingresoValor  * 1.35;
+    }
+
+    if(primerValor ==  'Peso Mexicano' && segundoValor == 'Euro' ){
+        valorSalida = ingresoValor / 23.67;
+    }
+    if(primerValor == 'Euro'  && segundoValor == 'Peso Mexicano' ){
+        valorSalida = ingresoValor  * 23.67;
+    }
+    if(primerValor ==  'Peso Mexicano' && segundoValor == 'Libra Esterlina' ){
+        valorSalida = ingresoValor / 27.98;
+    }
+    if(primerValor == 'Libra Esterlina' && segundoValor == 'Peso Mexicano' ){
+        valorSalida = ingresoValor  * 27.67;
+    }
+    if(primerValor == 'Euro'  && segundoValor == 'Libra Esterlina' ){
+        valorSalida = ingresoValor / 1.18;
+    }
+    if(primerValor == 'Libra Esterlina' && segundoValor == 'Euro'){
+        valorSalida = ingresoValor  * 1.18;
+    }
+
+
+    document.getElementById("resultado").innerText = valorSalida;
+}
+
+const moneda = ['Dolar', 'Peso Mexicano', 'Peso Colombiano', 'Euro', 'Libra Esterlina'];
+let seleccionarMoneda = document.getElementById("dropdown");//obtengo el select y lo asigno a una variable
+let seleccionarMoneda2 = document.getElementById("dropdown2");//obtengo el select y lo asigno a una variable
+
+const fragment = document.createDocumentFragment();
+moneda.forEach((item, index, arr) => {
+    console.log('[' + index + '] = ' + item);
+    let option = document.createElement("option");//creo un elemento de tipo option
+    option.setAttribute("value", item);
+    option.innerText = item;
+    fragment.appendChild(option);
+   
+});
+seleccionarMoneda.appendChild(fragment);//agrego el option al select
+
+const fragment2 = document.createDocumentFragment(); //crear fragmento de documento (contenedor, guarda las opciones antes de agregarlas al select)
+moneda.forEach((item, index, arr) => {
+    console.log('[' + index + '] = ' + item);
+    let option = document.createElement("option");//creo un elemento de tipo option
+    option.setAttribute("value", item);
+    option.innerText = item;
+    fragment2.appendChild(option);//se agregó al contenedor temporal
+});
+seleccionarMoneda2.appendChild(fragment2);//se agregó todas las opciones al tiempo
+
+
+
